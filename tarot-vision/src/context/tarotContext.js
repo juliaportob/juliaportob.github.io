cd7584422ef6ca1babbbb3a3171c9405185b25f4
Julia Porto <juportob@gmail.com>
2020-12-23 18:23:04 -0200

    feat: chage everything

--- tarot-vision/src/context/tarotContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const tarotContext = createContext();
-
-export default tarotContext;
--- tarot-vision/src/services/GetAPI.js
+++
@@ -1,9 +0,0 @@
-const API = 'https://rws-cards-api.herokuapp.com/api/v1/cards/random';
-
-export default async function fetchTarot () {
-  const returnAPI = await fetch(API);
-  const jsonAPI = await returnAPI.json();
-  const resultAPI = jsonAPI.cards[0];
-  console.log('resultado api', resultAPI)
-  return resultAPI;
-};
